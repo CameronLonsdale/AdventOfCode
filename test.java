11fe15d800c73bb22ee66ddf5d9af775ff1a8dcc
Cameron Lonsdale <Cameron.Lonsdale@gmail.com>
2023-05-29 11:00:12 +1000

    Create test.java

---
+++ test.java
@@ -0,0 +1,20 @@
+public class test {
+  public static void testing()
+      throws ForbiddenException {
+    if (a.equals(b)) {
+      return;
+    }
+   
+    if (c.equals(d)) {
+      return;
+    }
+   
+    if (e.equals(f)) {
+      return;
+    }
+    
+    if (g.equals(ch)) {
+      return;
+    }
+  }
+}
